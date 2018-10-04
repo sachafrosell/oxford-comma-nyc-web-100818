1b82bf354b25465b9f2a2b52552152b6d5a68d9f
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 01:10:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,20 +1,10 @@
-
-
 def oxford_comma(array)
-   if array.length == 1 
-    return array.join("")
-  elsif array.length == 2 
-    return "#{array.shift()} and #{array.pop()}"
-  else
-    counter = 1
-    new_array = []
-    until counter > array.length + 2
-      new_array.push(array.shift())
-      counter += 1 
-    end
-      puts new_array
-      return "#{new_array.join(", ")}, and #{array.join}"
-    end
+  if array.length == 1
+    return "#{array[0]}"
+  elsif array.length == 2
+    return array.join(" and ")
+  elsif array.length >= 3
+    array[-1] = "and #{array[-1]}"
+    return array.join(", ")
+  end
 end
-
-oxford_comma(["kiwi", "durian", "starfruit", "lemon", "hazze"])
