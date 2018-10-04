7241ef5ad68de69daa934ba7a524b2b936e2ff01
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 00:55:10 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -10,3 +10,15 @@ end
 oxford_comma(["harry", "john", "phillip"])
 
 def oxford_comma(array)
+   if array.length == 1 
+    return array.join("")
+  else
+    counter = 1
+    new_array = []
+    while counter < array.length
+      new_array.push(array.pop())
+      counter += 1 
+    end
+      return new_array
+    end
+end
