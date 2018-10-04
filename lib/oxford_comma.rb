06f6deb0d48f8c497aee7349a0eea72a662ed595
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 00:47:28 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,8 +1,9 @@
 def oxford_comma(array)
-  array.insert()
-    
-  string = array.join(", ")
-  
+  if array.length == 1 
+    return array.join("")
+  else 
+    array.insert(array.length - 1, "and")
+    return array.join(", ")
 end
 
 oxford_comma(["harry", "john", "phillip"])
