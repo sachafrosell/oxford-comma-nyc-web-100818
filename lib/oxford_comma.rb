3b97fdf37c31f3f5fb324e8ef4113be94c7e1036
Alexander Frosell <sachafrosell@gmail.com>
2018-10-04 00:57:26 +0000

    automatically backed up by learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,13 +1,4 @@
-def oxford_comma(array)
-  if array.length == 1 
-    return array.join("")
-  else 
-    array.insert(array.length - 1, "and")
-    return array.join(" ")
-  end
-end
 
-oxford_comma(["harry", "john", "phillip"])
 
 def oxford_comma(array)
    if array.length == 1 
@@ -19,7 +10,7 @@ def oxford_comma(array)
       new_array.push(array.shift())
       counter += 1 
     end
-      return new_array.join(", ")
+      return "#{new_array.join(", ")}"
     end
 end
 
